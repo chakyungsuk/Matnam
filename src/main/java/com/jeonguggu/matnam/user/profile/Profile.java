8f00fce3d99d74e13f92292beecf3b2d6d1bf046
hanyer9702 <hanyer9702@naver.com>
2022-05-06 16:08:03 +0900

    profile dto

---
+++ src/main/java/com/jeonguggu/matnam/user/profile/Profile.java
@@ -0,0 +1,30 @@
+package com.jeonguggu.matnam.user.profile;
+
+public class Profile {
+	
+	private String mnMmSeq = "";
+	private String mnMmId = "";
+	private String mnMmPassword = "";
+	
+	public String getMnMmSeq() {
+		return mnMmSeq;
+	}
+	public void setMnMmSeq(String mnMmSeq) {
+		this.mnMmSeq = mnMmSeq;
+	}
+	public String getMnMmId() {
+		return mnMmId;
+	}
+	public void setMnMmId(String mnMmId) {
+		this.mnMmId = mnMmId;
+	}
+	public String getMnMmPassword() {
+		return mnMmPassword;
+	}
+	public void setMnMmPassword(String mnMmPassword) {
+		this.mnMmPassword = mnMmPassword;
+	}
+	
+
+	
+}
