9fbeeeb0dd28acd8e4da91b3ff3f10ec7d56fc3f
chakyungsuk <cha0054@naver.com>
2022-05-06 17:28:29 +0900

    서비스 dto, 서비스 db

---
+++ src/main/java/com/jeonguggu/matnam/xmin/service/Service.java
@@ -0,0 +1,123 @@
+package com.jeonguggu.matnam.xmin.service;
+
+import java.util.Date;
+
+public class Service {
+
+	//matnamServices
+	private String mnsvSeq;
+	private String mnsvTitle;
+	private Date mnsvDateReceipt;
+	private Integer mnsvDoneNy;
+	private String mnsvDesc;
+	private Integer mnsvDelNy;
+	
+	//matnamServicesComplaint
+	private String mnscSeq;
+	private Integer mnscTypeCd;
+	private String mnscDesc;
+	private Integer mnscDelNy;
+
+	//matnamServicesReply
+	private String mnsrSeq;
+	private String mnsrContent;
+	private Date mnsrDate;
+	private String mnsrDesc;
+	private Integer mnsrDelNy;
+	
+	//=================================
+	
+	
+	public String getMnsvSeq() {
+		return mnsvSeq;
+	}
+	public void setMnsvSeq(String mnsvSeq) {
+		this.mnsvSeq = mnsvSeq;
+	}
+	public String getMnsvTitle() {
+		return mnsvTitle;
+	}
+	public void setMnsvTitle(String mnsvTitle) {
+		this.mnsvTitle = mnsvTitle;
+	}
+	public Date getMnsvDateReceipt() {
+		return mnsvDateReceipt;
+	}
+	public void setMnsvDateReceipt(Date mnsvDateReceipt) {
+		this.mnsvDateReceipt = mnsvDateReceipt;
+	}
+	public Integer getMnsvDoneNy() {
+		return mnsvDoneNy;
+	}
+	public void setMnsvDoneNy(Integer mnsvDoneNy) {
+		this.mnsvDoneNy = mnsvDoneNy;
+	}
+	public String getMnsvDesc() {
+		return mnsvDesc;
+	}
+	public void setMnsvDesc(String mnsvDesc) {
+		this.mnsvDesc = mnsvDesc;
+	}
+	public Integer getMnsvDelNy() {
+		return mnsvDelNy;
+	}
+	public void setMnsvDelNy(Integer mnsvDelNy) {
+		this.mnsvDelNy = mnsvDelNy;
+	}
+	public String getMnscSeq() {
+		return mnscSeq;
+	}
+	public void setMnscSeq(String mnscSeq) {
+		this.mnscSeq = mnscSeq;
+	}
+	public Integer getMnscTypeCd() {
+		return mnscTypeCd;
+	}
+	public void setMnscTypeCd(Integer mnscTypeCd) {
+		this.mnscTypeCd = mnscTypeCd;
+	}
+	public String getMnscDesc() {
+		return mnscDesc;
+	}
+	public void setMnscDesc(String mnscDesc) {
+		this.mnscDesc = mnscDesc;
+	}
+	public Integer getMnscDelNy() {
+		return mnscDelNy;
+	}
+	public void setMnscDelNy(Integer mnscDelNy) {
+		this.mnscDelNy = mnscDelNy;
+	}
+	public String getMnsrSeq() {
+		return mnsrSeq;
+	}
+	public void setMnsrSeq(String mnsrSeq) {
+		this.mnsrSeq = mnsrSeq;
+	}
+	public String getMnsrContent() {
+		return mnsrContent;
+	}
+	public void setMnsrContent(String mnsrContent) {
+		this.mnsrContent = mnsrContent;
+	}
+	public Date getMnsrDate() {
+		return mnsrDate;
+	}
+	public void setMnsrDate(Date mnsrDate) {
+		this.mnsrDate = mnsrDate;
+	}
+	public String getMnsrDesc() {
+		return mnsrDesc;
+	}
+	public void setMnsrDesc(String mnsrDesc) {
+		this.mnsrDesc = mnsrDesc;
+	}
+	public Integer getMnsrDelNy() {
+		return mnsrDelNy;
+	}
+	public void setMnsrDelNy(Integer mnsrDelNy) {
+		this.mnsrDelNy = mnsrDelNy;
+	}
+	
+	
+}
