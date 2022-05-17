e1884e83705ecc79360772c421f341b3554d7542
hanyer9702 <hanyer9702@naver.com>
2022-05-17 09:59:09 +0900

    시간 유틸 / 상수 파일

---
+++ src/main/java/com/jeonguggu/matnam/common/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.jeonguggu.matnam.common.constants;
+
+public final class Constants {
+	
+	public static final String DATETIME_FORMAT_BASIC = "yyyy-MM-dd HH:mm:ss";
+	public static final String DATE_FORMAT_BASIC = "yyyy-MM-dd";
+	
+	public static final int DATE_INTERVAL = -7;
+}
---
+++ src/main/java/com/jeonguggu/matnam/common/util/UtilDateTime.java
@@ -0,0 +1,53 @@
+package com.jeonguggu.matnam.common.util;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import com.jeonguggu.matnam.common.constants.Constants;
+
+public class UtilDateTime {
+
+	public static LocalDateTime nowLocalDateTime () throws Exception {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		return localDateTime;
+	}
+	
+
+	public static Date nowDate () throws Exception {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		String localDateTimeString = localDateTime.format(DateTimeFormatter.ofPattern(Constants.DATETIME_FORMAT_BASIC));
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(Constants.DATETIME_FORMAT_BASIC);
+        Date date = simpleDateFormat.parse(localDateTimeString);
+		return date;
+	}
+	
+	
+	public static String nowString () throws Exception {
+		LocalDateTime localDateTime = LocalDateTime.now();
+		String localDateTimeString = localDateTime.format(DateTimeFormatter.ofPattern(Constants.DATETIME_FORMAT_BASIC));
+		return localDateTimeString;
+	}
+	
+	public static String calculateDayString (LocalDateTime localDateTime, int day) throws Exception {
+		LocalDateTime localDateTimeNew;
+		
+		if(day >= 0) {
+				localDateTimeNew = localDateTime.plusDays(Math.abs(day)); 
+		} else {
+			localDateTimeNew = localDateTime.minusDays(Math.abs(day));
+		}
+
+		String localDateTimeNewString = localDateTimeNew.format(DateTimeFormatter.ofPattern(Constants.DATE_FORMAT_BASIC));
+		return localDateTimeNewString;
+	}
+	
+	public static String addStringTime(String date) {
+		return date + " 00:00:00";
+	}
+	
+	public static String addStringTimeNight(String date) {
+		return date + " 23:59:59";
+	}
+}
